25027b80c6e5c2461ac1408d355ac996901f675c
rivero-3 <marco3003207@gmail.com>
2025-04-21 19:40:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('/topic-listing', function () {
+    return view('pages.topic_listing');
+});
+
+Route::get('/', function () {
+    return view('pages.index');
+});
