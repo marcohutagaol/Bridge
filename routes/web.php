d777e9c27d82712ee5fa8c0de5e1fb7fb5216a08
sanjossye <sanjossye@gmail.com>
2025-06-03 19:30:18 +0700

    gasss

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
 
 
 
+
 //online deggre
 Route::get('/universities', [UniversityController::class, 'index'])->name('universities.index');
 Route::get('/module', [UniversityController::class, 'index'])->name('module.detail');
@@ -48,19 +49,32 @@ Route::get('/program/postgraduate', [UniversityController::class, 'postgraduate'
 Route::get('/exam', [CareerController::class, 'showCareers'])->name('careers');
 Route::get('/career/{id}', [CareerController::class, 'show'])->name('career.detail');
 //certifikat
+
 Route::get('/certificate-detail', [CourseController::class, 'index'])->name('certificate.detail');
 Route::get('/certificate-detail/{id}', [CourseController::class, 'show'])->name('certificate.detail.show');
+     // Halaman checkout
 Route::get('/course/{id}/checkout', [CourseController::class, 'checkout'])->name('course.checkout');
+
+// Proses checkout
+Route::get('/course/{id}/checkout', [CheckoutController::class, 'showCourseCheckout'])
+    ->name('course.checkout')
+    ->middleware('auth');
+
+// Route untuk checkout careers
 Route::get('/career/{id}/checkout', [CheckoutController::class, 'showCareerCheckout'])
     ->name('career.checkout')
     ->middleware('auth');
+
+// Routes untuk checkout - Method baru (recommended)
 Route::get('/{itemType}/{itemId}/checkout', [CheckoutController::class, 'show'])
     ->where(['itemType' => 'course|career|module', 'itemId' => '[0-9]+'])
     ->name('checkout.show');
+
 // Routes untuk backward compatibility - Method lama
 Route::get('/course/{courseId}/checkout', [CheckoutController::class, 'showCourseCheckout'])
     ->where('courseId', '[0-9]+')
     ->name('course.checkout');
+
 Route::get('/career/{careerId}/checkout', [CheckoutController::class, 'showCareerCheckout'])
     ->where('careerId', '[0-9]+')
     ->name('career.checkout');
@@ -77,6 +91,7 @@ Route::get('/my-learning', [MyLearningController::class, 'index'])->name('mylear
 Route::get('/next', function () {
     return view('pages.detail.nextkursus.learning_goals');
 });
+
 
 
 
