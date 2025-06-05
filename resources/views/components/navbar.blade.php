360e9523661f40753785d6e973e125de044b9f5d
Aryslaaan <aryalukmana32@gmail.com>
2025-06-05 14:34:59 +0700

    ganti UI profil lupa ganti url

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -54,9 +54,6 @@
 
             <div class="d-none d-lg-block">
                 <div class="dropdown">
-<<<<<<< HEAD
-                    <a href="/profile" class="navbar-icon bi-person smoothscroll"></a>
-=======
                     @if(Auth::check())
                         <a class="nav-link dropdown-toggle text-white" href="#" id="adminDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
@@ -68,7 +65,6 @@
                         </a>
                     @endif
 
->>>>>>> dcde238c76372be0832f78ef50254f2eab571649
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="/profile">Profil</a></li>
                         <li>
