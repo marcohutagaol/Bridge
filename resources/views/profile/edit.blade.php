a2948d7410289041f79a12a6a08775ff4a8497e6
stephenalbert146 <stephenalbert146@gmail.com>
2025-06-12 22:42:12 +0700

    perbaiki tampilan update profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
-
-     <div class="py-10 bg-gray-100 min-h-screen">
-        <div class="max-w-4xl mx-auto px-4 space-y-8">
-            <div class="bg-white rounded-lg shadow p-8">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.update-profile-information-form')
             </div>
-            <div class="bg-white rounded-lg shadow p-8">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.update-password-form')
             </div>
-            <div class="bg-white rounded-lg shadow p-8">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.delete-user-form')
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/profil', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name(name: 'profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
